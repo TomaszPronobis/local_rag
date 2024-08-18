f8efd6cda905fe0a77b7703237eb1f4cbf8e01eb
TomaszPronobis <tomasz.p.pronobis@gmail.com>
2024-08-18 22:14:22 +0200

    init llama-index

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import ollama
 from dotenv import load_dotenv
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
 import os
 
 # see: https://github.com/ollama/ollama-python/tree/main
@@ -23,3 +24,7 @@ options={
 
 # print(response['message']['content'])
 print(ollama.list())
+
+# llamaindex with ollama docs:
+# https://docs.llamaindex.ai/en/stable/examples/llm/ollama/
+
