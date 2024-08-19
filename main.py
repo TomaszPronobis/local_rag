8625d41a32f5ece2d0d0c4a2e24675dc1a54a162
TomaszPronobis <tomasz.p.pronobis@gmail.com>
2024-08-19 21:50:07 +0200

    load first directory

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ import os
 # git
 # set up ruff linting
 
+DIR = "~/Documents/nauka/ML_Literatur/paper/"
+
 load_dotenv()
 MODEL = os.getenv("MODEL")
 
@@ -28,3 +30,10 @@ print(ollama.list())
 # llamaindex with ollama docs:
 # https://docs.llamaindex.ai/en/stable/examples/llm/ollama/
 
+reader = SimpleDirectoryReader(input_dir=DIR)  # maybe use llama-parse instead of simpledirectory reader
+docs = reader.load_data()
+print(docs[0])
+
+# llamaindex:
+# advanced rag example:
+# https://github.com/run-llama/llama_parse/blob/main/examples/demo_advanced.ipynb
