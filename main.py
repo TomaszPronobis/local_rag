3360d66dfec15f423741eb49155b48575b3ef8e4
TomaszPronobis <tomasz.p.pronobis@gmail.com>
2024-08-21 22:26:03 +0200

    in memory vectorstore

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import ollama
 from dotenv import load_dotenv
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
+from llama_index.llms.ollama import Ollama
 import os
 
 # see: https://github.com/ollama/ollama-python/tree/main
@@ -13,16 +14,24 @@ DIR = "~/Documents/nauka/ML_Literatur/paper/"
 load_dotenv()
 MODEL = os.getenv("MODEL")
 
-response = ollama.chat(model=MODEL, messages=[
-  {
-    'role': 'user',
-    'content': 'Why is the sky blue?',
-  },
-],
-options={
-    "temperature": 1.5
-}
-)
+llm = Ollama(model=MODEL, request_timeout=120.0)
+resp = llm.complete("Who is Paul Graham?")
+print(resp)
+
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+
+embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
+
+# response = ollama.chat(model=MODEL, messages=[
+#   {
+#     'role': 'user',
+#     'content': 'Why is the sky blue?'
+#   },
+# ],
+# options={
+#     "temperature": 1.5
+# }
+# )
 
 # print(response['message']['content'])
 print(ollama.list())
@@ -33,6 +42,11 @@ print(ollama.list())
 reader = SimpleDirectoryReader(input_dir=DIR)  # maybe use llama-parse instead of simpledirectory reader
 docs = reader.load_data()
 print(docs[0])
+
+index = VectorStoreIndex.from_documents(docs)
+query_engine = index.as_query_engine()
+response = query_engine.query("Who wrote the playing Atari with Deep Learning Paper?")
+print(response)
 
 # llamaindex:
 # advanced rag example:
