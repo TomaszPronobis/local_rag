ce8332047aa326da3820dd874689f19213868b03
TomaszPronobis <tomasz.p.pronobis@gmail.com>
2024-08-25 20:58:13 +0200

    clean up & play around

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import ollama
 from dotenv import load_dotenv
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
 from llama_index.llms.ollama import Ollama
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.core import Settings
 import os
 
 # see: https://github.com/ollama/ollama-python/tree/main
@@ -9,21 +10,18 @@ import os
 # git
 # set up ruff linting
 
-DIR = "~/Documents/nauka/ML_Literatur/paper/"
-
 load_dotenv()
 MODEL = os.getenv("MODEL")
+DIR = os.getenv("DIR")
 
 llm = Ollama(model=MODEL, request_timeout=120.0)
-resp = llm.complete("Who is Paul Graham?")
-print(resp)
-
-from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+# resp = llm.complete("Who is Paul Graham?")
+# print(resp)
 
 embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
 
-
-from llama_index.core import Settings
+# consider using optimum & insatruct embeddings: 
+# https://docs.llamaindex.ai/en/stable/examples/embeddings/huggingface/
 
 Settings.llm = llm
 Settings.embed_model = embed_model
@@ -45,13 +43,15 @@ Settings.embed_model = embed_model
 # llamaindex with ollama docs:
 # https://docs.llamaindex.ai/en/stable/examples/llm/ollama/
 
-reader = SimpleDirectoryReader(input_dir=DIR)  # maybe use llama-parse instead of simpledirectory reader
+reader = SimpleDirectoryReader(input_dir=DIR, recursive=False)  # maybe use llama-parse instead of simpledirectory reader
 docs = reader.load_data()
-print(docs[0])
+# print(docs[0])
 
 index = VectorStoreIndex.from_documents(docs)
 query_engine = index.as_query_engine()
-response = query_engine.query("Who wrote the playing Atari with Deep Learning Paper?")
+query = "Explain if Tomasz Pronobis would be a good fit for the open position at DeepSpin!"
+print(query)
+response = query_engine.query(query)
 print(response)
 
 # llamaindex:
